9c44b2860e25dfd7a2a569a54ba046286b176287
Jasonarea <hyeonsuns@naver.com>
2019-08-14 16:05:52 +0900

    #1 - Daily Coding Backtracking Practice "Division"

---
+++ Backtracking/Division.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+using namespace std;
+
+// Division
+int res[101];
+int n;
+int cot = 0;
+
+void division(int index, int sum, int res[]) {
+    // base condition
+    if(sum == n) {
+        for(int i = 0;i<index - 1;i++)
+            printf("%d+", res[i]);
+        printf("%d", res[index-1]);
+        puts("");
+        cot++;
+        return;
+    }
+    else if(sum > n) return;
+
+    // recursive
+    for(int i = n-1;i>0;i--) {
+        bool isPossible = true;
+        for(int j = 0;j<index;j++) {
+            if(res[j] < i) isPossible = false;
+        }
+        if(!isPossible) continue;
+        res[index] = i;
+        sum += i;
+        division(index + 1, sum, res);
+        sum -= i;
+    }
+}
+
+int main(void)
+{
+    scanf("%d", &n);
+
+    int index = 0, sum = 0;
+    division(index, sum, res);
+    printf("%d\n", cot);
+
+    return 0;
+}
