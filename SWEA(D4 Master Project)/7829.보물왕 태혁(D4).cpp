c735be916ff1df1cc0ff6725a8c272a879924d9c
Jasonarea <hyeonsuns@naver.com>
2019-08-27 14:16:23 +0900

    20190827 Daily Coding #3 : SWEA D4 7829 보물왕 태혁(Math) (수정)

--- SWEA(D4 Master Project)/7829.보물왕 태혁(D4).cpp
+++ SWEA(D4 Master Project)/7829.보물왕 태혁(D4).cpp
@@ -1,4 +1,25 @@
-//
-// Created by 정현선 on 27/08/2019.
-//
+#include <cstdio>
+#include <algorithm>
+using namespace std;
 
+int main(void)
+{
+    int T;
+    scanf("%d", &T);
+
+    for(int tt = 1;tt<=T;tt++) {
+        int n;
+        scanf("%d", &n);
+        int *arr = new int[n];
+
+        for(int i = 0;i<n;i++)
+            scanf("%d", &arr[i]);
+
+        sort(arr, arr + n);
+
+        int ans = arr[0] * arr[n-1];
+        printf("#%d %d\n", tt, ans);
+    }
+
+    return 0;
+}
