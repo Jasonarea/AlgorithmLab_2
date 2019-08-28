b8394d2db011d24bde9622f2a310c9b8be56f779
Jasonarea <hyeonsuns@naver.com>
2019-08-28 10:10:15 +0900

    20190828 Daily Coding : SWEA D4 7733 치즈 도둑(BFS)(수정)

--- SWEA(D4 Master Project)/7733.치즈 도둑(D4).cpp
+++ SWEA(D4 Master Project)/7733.치즈 도둑(D4).cpp
@@ -55,7 +55,7 @@ int main(void)
         }
 
         int maxCnt = 0;
-        for(int k = 1;k<=maxFlavor;k++) {
+        for(int k = 0;k<=maxFlavor;k++) {
             int cot = 0;
             memset(check, 0, sizeof(check));
             for (int i = 0; i < n; i++) {
